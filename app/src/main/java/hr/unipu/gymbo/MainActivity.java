7e6957bd8b6b46bffd0e0742ba83d6253bdbb456
Adis Hadzic <adis7hadzic@gmail.com>
2022-05-18 00:43:25 +0200

    first commit

---
+++ app/src/main/java/hr/unipu/gymbo/MainActivity.java
@@ -0,0 +1,28 @@
+package hr.unipu.gymbo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Gymbo");
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+}
